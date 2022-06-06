283dbce21297884dfda1903c429188d4bf306973
EbbaStrandholm <ebba@strandholm.com>
2022-06-06 20:08:03 +0000

    Make it so the questions and options for the questions appear as paired, in the correct order and so that the next button sends the user to the next question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,6 +4,7 @@ const rules_box = document.querySelector(".rules_box");
 const button_quit = rules_box.querySelector(".buttons .button_quit");
 const button_restart = rules_box.querySelector(".buttons .button_restart");
 const quiz_box = document.querySelector(".quiz_box");
+const next = quiz_box.querySelector(".next");
 
 //If start quiz button is clicked
 start_button.onclick = ()=>{
@@ -19,4 +20,26 @@ button_quit.onclick = ()=>{
 button_restart.onclick = ()=>{
     rules_box.classList.remove("activeRules"); //The rules pop-up window will hide
     quiz_box.classList.add("activeQuiz"); //The quiz window will appear
+    showQuestions(0);
+}
+
+//If next button is clicked
+next.onclick = ()=>{
+    que_count++;
+    showQuestions(que_count);
+}
+
+let que_count = 0;
+
+//Taking and using questions and answers from array
+function showQuestions(index){
+    const question_text = document.querySelector(".question_text");
+    const answer_list = document.querySelector(".answer_list");
+    let que_tag = '<span>'+ questions[index].question +'</span>'
+    let option_tag = '<div class="answer">'+ questions[index].options[0] +'<span></span></div>'
+                    +'<div class="answer">'+ questions[index].options[1] +'<span></span></div>'
+                    +'<div class="answer">'+ questions[index].options[2] +'<span></span></div>'
+                    +'<div class="answer">'+ questions[index].options[3] +'<span></span></div>';
+    question_text.innerHTML = que_tag
+    answer_list.innerHTML = option_tag
 }
