c4bde78e713ba86c7d3a22ad257569c77f603c14
EbbaStrandholm <ebba@strandholm.com>
2022-06-06 18:15:58 +0000

    Javascript, add 4 const for start_button, rules_box, button_quit, button_continue

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+//Adding all required elements
+const start_button = document.querySelector(".start_button button");
+const rules_box = document.querySelector(".rules_box");
+const button_quit = rules_box.querySelector(".buttons .button_quit");
+const button_continue = document.querySelector(".buttons .button_restart");
+
