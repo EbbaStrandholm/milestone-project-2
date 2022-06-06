7a627f6a83762080b10a82743265b687f172bb33
EbbaStrandholm <ebba@strandholm.com>
2022-06-06 18:20:05 +0000

    Add a function so that clicking on the start button shows the rules pop-up window

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,7 @@ const rules_box = document.querySelector(".rules_box");
 const button_quit = rules_box.querySelector(".buttons .button_quit");
 const button_continue = document.querySelector(".buttons .button_restart");
 
+//If start quiz button is clicked
+start_button.onclick = ()=>{
+    rules_box.classList.add("activeRules");
+}
