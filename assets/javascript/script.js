05a948279550686e240bc9b1edef6d22a4d4160c
EbbaStrandholm <ebba@strandholm.com>
2022-06-06 18:48:08 +0000

    The quiz-box will appear and the rules-box will hide when continue is clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,8 @@
 const start_button = document.querySelector(".start_button button");
 const rules_box = document.querySelector(".rules_box");
 const button_quit = rules_box.querySelector(".buttons .button_quit");
-const button_continue = document.querySelector(".buttons .button_restart");
+const button_restart = rules_box.querySelector(".buttons .button_restart");
+const quiz_box = document.querySelector(".quiz_box");
 
 //If start quiz button is clicked
 start_button.onclick = ()=>{
@@ -11,5 +12,11 @@ start_button.onclick = ()=>{
 
 //If quit quiz button is clicked
 button_quit.onclick = ()=>{
-    rules_box.classList.remove("activeRules"); //The rules pop-up window will hide again
+    rules_box.classList.remove("activeRules"); //The rules pop-up window will hide
+}
+
+//If continue quiz button is clicked
+button_restart.onclick = ()=>{
+    rules_box.classList.remove("activeRules"); //The rules pop-up window will hide
+    quiz_box.classList.add("activeQuiz"); //The quiz window will appear
 }
